99f01b6da53ac3aa984c7bb70133c52d71a1684b
DylanGygax <dylangygax@ncf.edu>
2020-09-17 09:48:02 -0500

    new screenshot page created, but it still needs some debugging for park input

--- src/config/protectedRoutes.js
+++ src/config/protectedRoutes.js
@@ -2,11 +2,13 @@ import React from "react"
 import { Switch, Route, Router, BrowserRouter } from "react-router-dom"
 import Example from "../pages/example"
 import Logout from '../pages/logout'
+import newScreenshot from "../pages/newScreenshot"
 
 export default(
     <BrowserRouter>
         <Switch>
             <Route exact path="/myrct/e" component={Example}/>
+            <Route exact path="/myrct/newscreenshot" component={newScreenshot}/>
         </Switch>
     </BrowserRouter>
 )
