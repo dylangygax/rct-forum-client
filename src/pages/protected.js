0a0a5aff807d66abf45b4c6e3aae84d3b65226b6
DylanGygax <dylangygax@ncf.edu>
2020-09-17 21:08:20 -0500

    routes protected behind login

--- src/pages/protected.js
+++ src/pages/protected.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import protectedRoutes from '../config/protectedRoutes'
+import Lost from './lost'
+import {UserContext} from '../UserContext'
 
 function Protected() {
-    return (
-        <div className="Protected">
-            {protectedRoutes}
-        </div>
-    )
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    console.log(loggedInUser)
+    if (loggedInUser) {
+        return (
+            <div className="Protected">
+                {protectedRoutes}
+            </div>
+        )
+    } else {
+        return(<Lost />)
+    }
 }
   
 export default Protected
